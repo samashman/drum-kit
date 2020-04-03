d7d03b6bd8bf85cd86846594b6569a49a9d313f8
Sam Ashman <greenashmn@gmail.com>
2020-04-03 12:17:06 -0700

    added sound functionality for each drum button

---
+++ data.js
@@ -0,0 +1,4 @@
+var mySound = "sounds/crash.mp3";
+var soundsCollection = [ new Audio("sounds/crash.mp3"), new Audio("sounds/kick-bass.mp3"), new Audio("sounds/snare.mp3"),
+new Audio("sounds/tom-1.mp3"), new Audio("sounds/tom-2.mp3"), new Audio("sounds/tom-3.mp3"),
+new Audio("sounds/tom-4.mp3")];
